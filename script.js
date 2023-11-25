7dbb92df60a2a90f7e07d097063b8cbd5c357ffb
Aditya011041 <127628730+Aditya011041@users.noreply.github.com>
2023-11-25 23:47:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -94,7 +94,7 @@ function createListItemsFromJSON(data) {
 }
 
 // Function to fetch data from JSON file
-fetch('http://127.0.0.1:5000/get_listings')
+fetch('https://ecupserver.vercel.app/get_listings')
   .then(response => response.json())
   .then(data => {
     console.log(data)
